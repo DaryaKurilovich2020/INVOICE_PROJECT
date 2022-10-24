2431d5a85b2326e543769da24f5c3fb8feac4e46
DaryaKurilovich2020 <kurilovich.dasha@yandex.ru>
2022-10-24 14:13:03 +0300

    PDF Report

--- src/aura/orderManagementQA/orderManagementQAController.js
+++ src/aura/orderManagementQA/orderManagementQAController.js
@@ -1,6 +1,10 @@
 ({
     handleSubmitClick : function (cmp, event, helper) {
-        //TODO Pdf send
-        console.log('pip');
+        var instanceUrl = cmp.get('v.meta.dto.instanceURL'),
+            orderId = cmp.get('v.meta.dto.order.Id'),
+            procName = event.getSource().getLocalId()
+        if (instanceUrl && procName && orderId) {
+            window.open(instanceUrl + '/apex/vertic_Content?proc=' + procName + '&renderAs=pdf' + '&orderId=' + orderId, '_blank');
+        }
     }
 });
